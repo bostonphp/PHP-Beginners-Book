7cca113120f5f09a74c3484323257e813e437263
Michael Bourque <michael.bourque@gmail.com>
2011-07-04 12:28:28 -0400

    - Added sample code

---
+++ learning_php/samples/loops.php
@@ -0,0 +1,24 @@
+	<?php // table.php
+
+	// Store the values in an array
+	$users[] = array( 1, 'Michael Bourque', 'michael@bostonphp.org');
+	$users[] = array( 2, 'Gene Babon', 'gene@bostonphp.org');
+	?>
+	<table>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Email</th>
+			<tr>
+		</thead>
+		<tbody>
+			<?php foreach( $users AS $user ) : ?>
+			<tr>
+				<?php foreach( $user AS $value ) : ?>
+				<td><?php echo $value; ?></td>
+				<?php endforeach; ?>
+			<tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
